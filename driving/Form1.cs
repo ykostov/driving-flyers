2dce1c9798a0bd77f4e45ee33be2944525fe9649
niki <nikolayp1707@gmail.com>
2021-12-31 14:45:12 +0200

    add QuestionChooser function and template for question values

--- driving/Form1.cs
+++ driving/Form1.cs
@@ -12,9 +12,160 @@ namespace driving
 {
     public partial class Form1 : Form
     {
+      int points;
+
         public Form1()
         {
             InitializeComponent();
         }
+    }
+    void QuestionChooser{
+          Random r = new Random();
+          int qn = r.Next(1,25);
+          if(qn == 1){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 2){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 3){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 4){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 5){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 6){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 7){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 8){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 9){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 10){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 11){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 12){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 13){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 14){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 16){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 17){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 18){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 19){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 20){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 21){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 22){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 23){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 24){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else if(qn == 25){
+            label1.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+          } else 
     }
 }
