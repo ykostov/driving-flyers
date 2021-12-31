de0e0fc959ad1f595705accaa9091b1eb82be5df
Yasen Kostov <ykostov@pm.me>
2021-12-31 06:26:46 -0800

    Init .net repo

---
+++ driving5/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace driving5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            QuestionChooser();
+        }
+
+        private void QuestionChooser()
+        {
+            Random r = new Random();
+            int qnum = r.Next(1, 1);
+
+            if (qnum == 1)
+            {
+                label1.Text = "New tex111t";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 2)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 3)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 4)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 5)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 6)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 7)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 8)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 9)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 10)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 11)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 12)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 13)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 14)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 16)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 17)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 18)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 19)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 20)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 21)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 22)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 23)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 24)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+            else if (qnum == 25)
+            {
+                label1.Text = "";
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "1111";
+        }
+    }
+}
