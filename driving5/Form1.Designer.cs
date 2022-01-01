49cacf9e506bebcf6737db941d3a89c35057f864
Yasen Kostov <ykostov@pm.me>
2022-01-01 08:55:35 -0800

    stylize form, add close button

--- driving5/Form1.Designer.cs
+++ driving5/Form1.Designer.cs
@@ -34,14 +34,14 @@
             this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button5 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(137, 255);
+            this.button1.Location = new System.Drawing.Point(44, 223);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(256, 60);
             this.button1.TabIndex = 0;
             this.button1.Tag = "1";
             this.button1.Text = "button1";
@@ -50,9 +50,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(137, 304);
+            this.button2.Location = new System.Drawing.Point(44, 305);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(256, 60);
             this.button2.TabIndex = 1;
             this.button2.Tag = "2";
             this.button2.Text = "button2";
@@ -61,9 +61,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(396, 255);
+            this.button3.Location = new System.Drawing.Point(421, 223);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(256, 60);
             this.button3.TabIndex = 2;
             this.button3.Tag = "3";
             this.button3.Text = "button3";
@@ -72,9 +72,9 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(396, 304);
+            this.button4.Location = new System.Drawing.Point(421, 305);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(256, 60);
             this.button4.TabIndex = 3;
             this.button4.Tag = "4";
             this.button4.Text = "button4";
@@ -83,35 +83,40 @@
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(284, 157);
+            this.label1.Location = new System.Drawing.Point(44, 68);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.Size = new System.Drawing.Size(633, 128);
             this.label1.TabIndex = 4;
             this.label1.Text = "question";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(395, 65);
+            this.button5.Location = new System.Drawing.Point(305, 392);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(110, 27);
             this.button5.TabIndex = 5;
-            this.button5.Text = "button5";
+            this.button5.Text = "NEXT";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
-            // textBox1
+            // button6
             // 
-            this.textBox1.Location = new System.Drawing.Point(238, 85);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 6;
+            this.button6.BackColor = System.Drawing.Color.Red;
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(44, 392);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 27);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "EXIT";
+            this.button6.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(718, 450);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
@@ -121,7 +126,6 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -133,7 +137,7 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
     }
 }
 
