4eff63b7a02a254ff6ec9e7e06380bd056755712
Yasen Kostov <ykostov@pm.me>
2022-01-02 03:58:22 -0800

    fix errors and other changes

--- driving5/Form1.Designer.cs
+++ driving5/Form1.Designer.cs
@@ -28,116 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.RadioButton();
             this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(44, 223);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(256, 60);
-            this.button1.TabIndex = 0;
-            this.button1.Tag = "1";
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(44, 305);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(256, 60);
-            this.button2.TabIndex = 1;
-            this.button2.Tag = "2";
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(421, 223);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(256, 60);
-            this.button3.TabIndex = 2;
-            this.button3.Tag = "3";
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(421, 305);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(256, 60);
-            this.button4.TabIndex = 3;
-            this.button4.Tag = "4";
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(44, 68);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 124);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(633, 128);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "question";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(86, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 17);
+            this.button1.TabIndex = 1;
+            this.button1.TabStop = true;
+            this.button1.Text = "radioButton1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Location = new System.Drawing.Point(422, 283);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 17);
+            this.button4.TabIndex = 2;
+            this.button4.TabStop = true;
+            this.button4.Text = "radioButton1";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(422, 205);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 17);
+            this.button3.TabIndex = 3;
+            this.button3.TabStop = true;
+            this.button3.Text = "radioButton1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(86, 283);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 17);
+            this.button2.TabIndex = 4;
+            this.button2.TabStop = true;
+            this.button2.Text = "radioButton1";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(305, 392);
+            this.button5.Location = new System.Drawing.Point(452, 40);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(110, 27);
+            this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 5;
-            this.button5.Text = "NEXT";
+            this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button6
-            // 
-            this.button6.BackColor = System.Drawing.Color.Red;
-            this.button6.ForeColor = System.Drawing.Color.White;
-            this.button6.Location = new System.Drawing.Point(44, 392);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(110, 27);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "EXIT";
-            this.button6.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(718, 450);
-            this.Controls.Add(this.button6);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton button1;
+        private System.Windows.Forms.RadioButton button4;
+        private System.Windows.Forms.RadioButton button3;
+        private System.Windows.Forms.RadioButton button2;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
     }
 }
 
